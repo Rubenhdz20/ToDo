a280c12b0dff20b9529ce92f0381835713815169
Rubenhdz20 <rubhernandez42@gmail.com>
2024-05-15 16:14:01 -0600

    Use localstorage to understand how it works

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,30 @@ import TodoList from "./components/TodoList";
 import TodoItem from "./components/TodoItem";
 import CreateTodoButton from "./components/CreateTodoButton";
 
-const defaultTodos = [
-  { text: 'Sacar a canelo', completed: false },
-  { text: 'Cocinar', completed: true },
-  { text: 'Limpiar cocina!', completed: false },
-  { text: 'Lavar los platos', completed: true },
-  { text: 'Lavar los regadera', completed: true }
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1');
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+
+  if(!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+  
+  const [todos, setTodos] = useState(parsedTodos);
 
   const [searchValue, setSearchValue] = useState('');
 
@@ -27,7 +41,12 @@ function App() {
     const todoText = todo.text.toLowerCase();
     const searchText = searchValue.toLowerCase();
     return todoText.includes(searchText);
-  })
+  });
+
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
 
   const completeTodo = (text) => {
     const newTodos = [...todos];
@@ -35,7 +54,7 @@ function App() {
       (todo) => todo.text === text
     );
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -44,7 +63,7 @@ function App() {
       (todo) => todo.text === text
     );
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,8 +4,8 @@ import { MdClose } from "react-icons/md";
 function TodoItem(props) {
     return(
         <li className="w-52 flex justify-between items-center">
-            <MdCheck className="hover:cursor-pointer hover:bg-green-500 hover:rounded-lg" onClick={props.onComplete}/>
-            <p className={props.completed &&  "line-through"}>{props.text}</p>
+            <MdCheck className="green-500 hover:cursor-pointer hover:bg-green-500 hover:rounded-lg" onClick={props.onComplete}/>
+            <p className={props.completed ? "line-through" : ""}>{props.text}</p>
             <MdClose className="hover:cursor-pointer hover:bg-red-500 hover:rounded-lg" onClick={props.onDelete}/>
         </li>
     )
